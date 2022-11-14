efa6e62113270a761dd40bfd847749506f76c569
astraeabg <98741593+astraeabg@users.noreply.github.com>
2022-11-14 14:12:16 +0100

    mas cambios

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-function openNav() {
-    document.getElementById("sideNavigation").style.width = "414px"
-    document.getElementById("main").style.marginLeft = "250px"
-}
+// function openNav() {
+//     document.getElementById("sideNavigation").style.width = "414px"
+//     document.getElementById("main").style.marginLeft = "250px"
+// }
  
-function closeNav() {
-    document.getElementById("sideNavigation").style.width = "0"
-    document.getElementById("main").style.marginLeft = "0"
-}
+// function closeNav() {
+//     document.getElementById("sideNavigation").style.width = "0"
+//     document.getElementById("main").style.marginLeft = "0"
+// }
