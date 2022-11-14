12b7e0c8b8f11fd4d49ef693b8ecc7a3440ccb5f
BelenCao <belencao1997@gmail.com>
2022-11-14 13:39:19 +0100

    Header

---
+++ main.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sideNavigation").style.width = "414px"
+    document.getElementById("main").style.marginLeft = "250px"
+}
+ 
+function closeNav() {
+    document.getElementById("sideNavigation").style.width = "0"
+    document.getElementById("main").style.marginLeft = "0"
+}
