de115808b282079c463d703af4765220f143feaf
MaribelLopezRomero <maribel.miv.romero@gmail.com>
2022-11-15 13:34:38 +0100

    header y menu

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
+// function openNav() {
+//     document.getElementById("mySidenav").style.width = "250px";
+//   }
   
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+//   function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+//   }
+
+
+const botonCerrar = document.querySelector(".closebtn");
+
+const menu =  document.querySelector(".sidenav");
+
+function CloseNav(){
+
+  menu.classList.add("hidden");
+}
+
+
+botonCerrar.addEventListener("click", CloseNav);
+
